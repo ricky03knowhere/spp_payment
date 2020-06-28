0f5caf5154d132fdbd0abb1816d7bd58fe24ab27
ricky03knowhere <67403273+ricky03knowhere@users.noreply.github.com>
2020-06-28 10:16:12 +0700

    Add files via upload

---
+++ assets/templates/footer.php
@@ -0,0 +1,46 @@
+</div>
+</div>
+<!-- End of Main Content -->
+
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+  <div class="container my-auto">
+    <div class="copyright text-center my-auto">
+      <span>Copyright &copy; <a href="https://github.com/ricky03knowhere" >ricky03knowhere</a> 2020</span>
+    </div>
+  </div>
+</footer>
+<!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="../vendor/admin/vendor/jquery/jquery.min.js"></script>
+  <script src="../vendor/admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="../vendor/admin/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="../vendor/admin/js/sb-admin-2.min.js"></script>
+  
+  <script src="../vendor/admin/vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="../vendor/admin/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+  
+  <!-- Page level custom scripts -->
+  <script src="../vendor/admin/js/demo/datatables-demo.js"></script>
+  <script src="../assets/js/sweetalert2.all.min.js"></script>
+  <script src="../assets/js/script.js"></script>
+</body>
+</html>
---
+++ assets/templates/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>SPP Payment</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="../assets/img/favicon.png" rel="icon" width="300px"  type="image/png">
+  <link href="../vendor/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <!-- Custom styles for this template-->
+  <link href="../vendor/admin/css/sb-admin-2.min.css" rel="stylesheet">
+  <style type="text/css">
+  .pict{
+		margin-right:-15.8px;
+  		padding-right:10px;
+  		padding-left:10px;
+  }
+  .img-profile {
+  		transform:scale(1.5);
+  }
+  .menu{
+	  border-radius:40px;
+	  height:50px;
+	  font-size:12px;
+  }
+  .insert {
+	  font-size:17px !important;
+	  font-weight:bold;
+	  width:100%;
+  }
+	.lbl {
+		font-size:17px;
+	}
+	.date-pick {
+		padding-bottom:40px !important;
+	}
+	.card-img-top {
+		transform:scale(.6);
+	}
+	.profile .list-group-item span{
+		position:absolute;
+		right: 20px;
+	}
+	.profile .h3{
+		margin-top:-40px;
+	}
+  </style>
+</head>
+<body id="page-top">
+  <!-- Page Wrapper -->
+  <div id="wrapper">
